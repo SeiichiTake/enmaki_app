fea0a718993e9d209852f43cd9c82e05c770bbd8
Seiichi Takeuchi <stakeuchibam@gmail.com>
2024-10-22 19:10:14 +0900

    Refactor code to use config values for dropdown options

--- app.py
+++ app.py
@@ -3,6 +3,14 @@ import pandas as pd
 import numpy as np
 import datetime
 
+from config import(
+    NAMES, AREAS, FISHES
+)
+
+name_list = NAMES
+area_list = AREAS
+fish_list = FISHES
+
 st.set_page_config(layout="wide") # ページのレイアウト設定
 
 def main():
@@ -22,14 +30,15 @@ def main():
     st.table(df_transfer)
     # 新規に余っている枠を譲渡申請する場合の機能
     st.write('新規枠譲渡申請をする')
-    new_transfer_name = st.selectbox('漁業者名を選択してください', ['漁業者A', '漁業者B', '漁業者C', '漁業者D'], key = 'new_transfer_name') # 将来的にはDBもしくはconfigから取得する
-    new_transfer_area = st.selectbox('海区を選択してください', ['海区1', '海区2'], key = 'new_transfer_area') # 将来的にはDBもしくはconfigから取得する
-    new_transfer_fish = st.selectbox('枠の魚種を選択してください', ['さば', 'まいわし', 'あじ'], key = 'new_transfer_fish') # 将来的にはDBもしくはconfigから取得する
+    new_transfer_name = st.selectbox('漁業者名を選択してください', name_list, key = 'new_transfer_name') # 将来的にはDBもしくはconfigから取得する
+    new_transfer_area = st.selectbox('海区を選択してください', area_list, key = 'new_transfer_area') # 将来的にはDBもしくはconfigから取得する
+    new_transfer_fish = st.selectbox('枠の魚種を選択してください', fish_list, key = 'new_transfer_fish') # 将来的にはDBもしくはconfigから取得する
     new_transfer_quota = st.number_input('枠の数量を入力してください', min_value=0, max_value=1000, value=0, key = 'new_transfer_quota')
     new_transfer_submit = st.button('新規枠譲渡申請を送信する')
     if new_transfer_submit:
         new_transfer_id = df_transfer['id'].max() + 1
-        new_transfer_data = pd.DataFrame({'id': [new_transfer_id], 'date': [datetime.datetime.now(datetime.timezone(datetime.timedelta(hours=9)))], 'name': [new_transfer_name], 'area': [new_transfer_area], 'fish': [new_transfer_fish], 'quota': [new_transfer_quota], 'status': ['transfer']})
+        formatted_date = datetime.datetime.now(datetime.timezone(datetime.timedelta(hours=9))).strftime('%Y/%m/%d')
+        new_transfer_data = pd.DataFrame({'id': [new_transfer_id], 'date': [formatted_date], 'name': [new_transfer_name], 'area': [new_transfer_area], 'fish': [new_transfer_fish], 'quota': [new_transfer_quota], 'status': ['transfer']})
         df = pd.concat([df, new_transfer_data], ignore_index=True)
         st.write('新規枠譲渡申請を送信しました')
         df.to_csv('data/data_sample.csv', index=False)
@@ -44,14 +53,15 @@ def main():
     st.table(df_get)
     # 新規に足りない枠を取得申請する場合の機能
     st.write('新規枠取得申請をする')
-    new_get_name = st.selectbox('漁業者名を選択してください', ['漁業者A', '漁業者B', '漁業者C', '漁業者D'], key = 'new_get_name') # 将来的にはDBもしくはconfigから取得する
-    new_get_area = st.selectbox('海区を選択してください', ['海区1', '海区2'], key = 'new_get_area') # 将来的にはDBもしくはconfigから取得する
-    new_get_fish = st.selectbox('枠の魚種を選択してください', ['さば', 'まいわし', 'あじ'], key = 'new_get_fish') # 将来的にはDBもしくはconfigから取得する
+    new_get_name = st.selectbox('漁業者名を選択してください', name_list, key = 'new_get_name') # 将来的にはDBもしくはconfigから取得する
+    new_get_area = st.selectbox('海区を選択してください', area_list, key = 'new_get_area') # 将来的にはDBもしくはconfigから取得する
+    new_get_fish = st.selectbox('枠の魚種を選択してください', fish_list, key = 'new_get_fish') # 将来的にはDBもしくはconfigから取得する
     new_get_quota = st.number_input('枠の数量を入力してください', min_value=0, max_value=1000, value=0, key = 'new_get_quota')
     new_get_submit = st.button('新規枠取得申請を送信する')
     if new_get_submit:
         new_get_id = df_get['id'].max() + 1
-        new_get_data = pd.DataFrame({'id': [new_get_id], 'date': [datetime.datetime.now(datetime.timezone(datetime.timedelta(hours=9)))], 'name': [new_get_name], 'area': [new_get_area], 'fish': [new_get_fish], 'quota': [new_get_quota], 'status': ['get']})
+        formatted_date = datetime.datetime.now(datetime.timezone(datetime.timedelta(hours=9))).strftime('%Y/%m/%d')
+        new_get_data = pd.DataFrame({'id': [new_get_id], 'date': [formatted_date], 'name': [new_get_name], 'area': [new_get_area], 'fish': [new_get_fish], 'quota': [new_get_quota], 'status': ['get']})
         df = pd.concat([df, new_get_data], ignore_index=True)
         st.write('新規枠取得申請を送信しました')
         df.to_csv('data/data_sample.csv', index=False)
