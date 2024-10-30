00d2e70ca25ab9f7e57332a494ccaee7ab3ad832
Seiichi Takeuchi <stakeuchibam@gmail.com>
2024-10-30 14:16:26 +0900

    Refactor code to use session_state for storing data

--- app.py
+++ app.py
@@ -41,51 +41,61 @@ def main():
             st.divider()
     
         # TODO: ユーザー情報を取得する
+        login_user = st.session_state["name"]
 
         # TODO: ページを分けるのであれば、session_stateを使って情報を保持する
 
         df = pd.read_csv('data/data_sample.csv') # 将来的にはDBから取得する.今はサンプルデータを使用
         # TODO: データをきれいに成型する必要あり
 
+        # session_stateにデータを保存
+        if 'df_transfer' not in st.session_state:
+            st.session_state.df_transfer = df[df['status'] == 'transfer']
+        if 'df_get' not in st.session_state:
+            st.session_state.df_get = df[df['status'] == 'get']
+
+        df_transfer = st.session_state.df_transfer
+        df_get = st.session_state.df_get
+
         st.markdown('## 余っている枠一覧')
-        df_transfer = df[df['status'] == 'transfer']
-        st.table(df_transfer)
+        st.table(df_transfer, hide_index=True)
         # 新規に余っている枠を譲渡申請する場合の機能
         st.write('新規枠譲渡申請をする')
-        new_transfer_name = st.selectbox('漁業者名を選択してください', name_list, key = 'new_transfer_name') # 将来的にはDBもしくはconfigから取得する
+        new_transfer_name = login_user
         new_transfer_area = st.selectbox('海区を選択してください', area_list, key = 'new_transfer_area') # 将来的にはDBもしくはconfigから取得する
         new_transfer_fish = st.selectbox('枠の魚種を選択してください', fish_list, key = 'new_transfer_fish') # 将来的にはDBもしくはconfigから取得する
         new_transfer_quota = st.number_input('枠の数量を入力してください', min_value=0, max_value=1000, value=0, key = 'new_transfer_quota')
         new_transfer_submit = st.button('新規枠譲渡申請を送信する')
         if new_transfer_submit:
-            new_transfer_id = df_transfer['id'].max() + 1
+            new_transfer_id = int(df_transfer['id'].max() + 1)
             formatted_date = str(datetime.datetime.now(datetime.timezone(datetime.timedelta(hours=9))).strftime('%Y/%m/%d'))
             new_transfer_data = pd.DataFrame({'id': [new_transfer_id], 'date': [formatted_date], 'name': [new_transfer_name], 'area': [new_transfer_area], 'fish': [new_transfer_fish], 'quota': [new_transfer_quota], 'status': ['transfer']})
-            df = pd.concat([df, new_transfer_data], ignore_index=True)
+            df_transfer = pd.concat([df_transfer, new_transfer_data], ignore_index=True)
+            st.session_state.df_transfer = df_transfer
             st.write('新規枠譲渡申請を送信しました')
+            df = pd.concat([df_transfer, df_get], ignore_index=True)
             df.to_csv('data/data_sample.csv', index=False)
             # ページをリロードする
             st.rerun(scope="app")
 
-
-        # TODO: 枠をもらいたい人の情報を表示
         st.divider()
         st.markdown('## 足りていない枠一覧')
-        df_get = df[df['status'] == 'get']
-        st.table(df_get)
+        st.table(df_get, hide_index=True)
         # 新規に足りない枠を取得申請する場合の機能
         st.write('新規枠取得申請をする')
-        new_get_name = st.selectbox('漁業者名を選択してください', name_list, key = 'new_get_name') # 将来的にはDBもしくはconfigから取得する
+        new_get_name = login_user
         new_get_area = st.selectbox('海区を選択してください', area_list, key = 'new_get_area') # 将来的にはDBもしくはconfigから取得する
         new_get_fish = st.selectbox('枠の魚種を選択してください', fish_list, key = 'new_get_fish') # 将来的にはDBもしくはconfigから取得する
         new_get_quota = st.number_input('枠の数量を入力してください', min_value=0, max_value=1000, value=0, key = 'new_get_quota')
         new_get_submit = st.button('新規枠取得申請を送信する')
         if new_get_submit:
-            new_get_id = df_get['id'].max() + 1
+            new_get_id = int(df_get['id'].max() + 1)
             formatted_date = str(datetime.datetime.now(datetime.timezone(datetime.timedelta(hours=9))).strftime('%Y/%m/%d'))
             new_get_data = pd.DataFrame({'id': [new_get_id], 'date': [formatted_date], 'name': [new_get_name], 'area': [new_get_area], 'fish': [new_get_fish], 'quota': [new_get_quota], 'status': ['get']})
-            df = pd.concat([df, new_get_data], ignore_index=True)
+            df_get = pd.concat([df_get, new_get_data], ignore_index=True)
+            st.session_state.df_get = df_get
             st.write('新規枠取得申請を送信しました')
+            df = pd.concat([df_transfer, df_get], ignore_index=True)
             df.to_csv('data/data_sample.csv', index=False)
             # ページをリロードする
             st.rerun(scope="app")
@@ -100,11 +110,13 @@ def main():
             select_get = st.selectbox('枠を選択してください', df_transfer['id'], key = 'select_get')
             if select_get is not None:
                 st.write('もらう枠の詳細情報')
-                st.table(df_transfer[df_transfer['id'] == select_get])
+                st.table(df_transfer[df_transfer['id'] == select_get], hide_index=True)
                 get_request = st.button('枠をもらう依頼を確定する')
                 if get_request:
                     st.write('枠をもらう依頼を確定しました')
-                    df.loc[df['id'] == select_get, 'status'] = 'got'
+                    df_transfer = df_transfer[df_transfer['id'] != select_get]
+                    st.session_state.df_transfer = df_transfer # session_stateにデータを保存
+                    df = pd.concat([df_transfer, df_get], ignore_index=True)
                     df.to_csv('data/data_sample.csv', index=False)
                     # ページをリロードする
                     st.rerun(scope="app")
@@ -113,11 +125,13 @@ def main():
             select_transfer = st.selectbox('枠を選択してください', df_get['id'])
             if select_transfer is not None:
                 st.write('譲る枠の詳細情報')
-                st.table(df_get[df_get['id'] == select_transfer])
+                st.table(df_get[df_get['id'] == select_transfer], hide_index=True)
                 transfer_request = st.button('枠を譲る依頼を確定する')
                 if transfer_request:
                     st.write('枠を譲る依頼を確定しました')
-                    df.loc[df['id'] == select_transfer, 'status'] = 'transferred'
+                    df_get = df_get[df_get['id'] != select_transfer]
+                    st.session_state.df_get = df_get
+                    df = pd.concat([df_transfer, df_get], ignore_index=True)
                     df.to_csv('data/data_sample.csv', index=False)
                     # ページをリロードする
                     st.rerun(scope="app")
